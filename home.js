4198dac8962b9da80e5f4dcfe350d5bdc9f3fe86
PreethamKolli2002 <preetham.cs20@bmsce.ac.in>
2022-02-15 00:25:50 +0530

    d

---
+++ home.js
@@ -0,0 +1,38 @@
+ /* var latest = document.getElementById('latest');
+var mostviewed = document.getElementById('most-viewed');
+ 
+latest.onclick = changeColor_latest;
+mostviewed.onclick = changeColor;   
+
+
+            function changeColor_latest() {
+                latest.style.color = "purple";
+                return false;
+            }    */
+
+/* latest.addEventListener('click',()=>{
+    latest.style.background =  color;
+})
+
+mostviewed.addEventListener('click',()=>{
+    mostviewed.style.background =  color;
+})  */
+
+/* var latest = document.getElementById("latest");
+var mostviewed = document.getElementById("most-viewed");
+          
+            function changeColor(color) {
+                document.body.style.background = color;
+            }
+              
+            function bg_Change() {
+                changeColor('blue');
+            }      */  
+            
+            /* document.getElementById('latest').onclick = changeColor;   
+
+            function changeColor() {
+                document.body.style.color = "purple";
+                return false;
+            }   
+         */
